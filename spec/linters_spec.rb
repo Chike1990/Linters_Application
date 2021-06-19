32882ff99c909b9b910545e0fb17d364f85eae57
Chike1990 <chibuikeuzoechina@gmail.com>
2021-06-19 20:25:30 +0100

    Fix rubocop

--- spec/linters_spec.rb
+++ spec/linters_spec.rb
@@ -1,16 +1,24 @@
-require_relative '../lib/rules.rb'
+require_relative '../lib/rules'
 
-describe linters do
-  describe Rules do
-    let(:rules) {Rules.new}
-    describe '#first_line' do
-        it 'returns a message to an array of error messages' do
-    
-      expect(error_scanner.first_line).to eql()
+RSpec.describe Rules do
+  describe '#first_line' do
+    it 'returns an array of error messages' do
+      first_line = 'json file test'
+      expect(Rules.new(first_line, 0).check_for_errors).to include(["'{' expected at the beginning of the line", 1])
+      # p Rules.new(first_line, 0).check_for_errors
+    end
+
+    it 'doen\'t return an array any error in case of the first line contains only \'{\'' do
+      first_line = '{'
+      expect(Rules.new(first_line, 0).check_for_errors).not_to include(["'{' expected at the beginning of the line", 1])
+      # p Rules.new(first_line, 0).check_for_errors
+    end
+  end
+
+  describe '#after_curly' do
+    it 'checks if there is a new line after \'{\'' do
+      first_line = '{'
+      expect(Rules.new(first_line, 0).check_for_errors).to include(["New line expected after '{'", 1])
     end
   end
 end
-end
-
-
-
